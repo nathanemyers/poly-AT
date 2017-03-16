be30e3d7a9ced9cfd101bb0e1f358ca2da570fc3
Nathan Myers <nathanemyers@gmail.com>
2017-03-16 08:53:39 -0500

    tests, they work now!

--- app/photo/models.py
+++ app/photo/models.py
@@ -7,7 +7,7 @@ from app.rgb.models import RGB
 
 class Photo(models.Model):
     name = models.CharField(max_length=255)
-    image = models.ImageField(height_field="height", width_field="width")
+    image = models.ImageField(height_field="height", width_field="width", upload_to="static/tmp")
     height = models.IntegerField()
     width = models.IntegerField()
     rgb = models.ForeignKey(RGB, blank=True, null=True)
--- app/photo/tests.py
+++ app/photo/tests.py
@@ -1,5 +1,6 @@
+import os
 from django.test import TestCase
-from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.files import File
 
 from app.photo.models import Photo
 from app.rgb.models import RGB
@@ -7,18 +8,15 @@ from app.rgb.models import RGB
 
 class PhotoTests(TestCase):
     def setUp(self):
+        test_image = File(open('static/test/woods.jpg'))
         self.photo = Photo()
-        self.photo.image = SimpleUploadedFile(
-            name='woods.jpg',
-            content=open('static/test/', 'rb').read(),
-            content_type='image/jpeg'
-        )
+        self.photo.image.save('test_image', test_image)
 
     def test_height(self):
-        self.assertEqual(self.photo.height, 0)
+        self.assertEqual(self.photo.height, 2448)
 
     def test_width(self):
-        self.assertEqual(self.photo.width, 0)
+        self.assertEqual(self.photo.width, 3264)
 
     def test_color_sumnation(self):
         self.assertEqual(self.photo.sum_color, RGB())
