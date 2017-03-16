75d474fccff0c80d6b80488b48907bfce917ee93
Nathan Myers <nathanemyers@gmail.com>
2017-03-15 20:00:46 -0500

    flubbin around with tests

--- app/photo/tests.py
+++ app/photo/tests.py
@@ -1,18 +1,28 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 from app.photo.models import Photo
 from app.rgb.models import RGB
 
 
 class PhotoTests(TestCase):
-    def set_up(self):
+    def setUp(self):
         self.photo = Photo()
+        self.photo.image = SimpleUploadedFile(
+            name='woods.jpg',
+            content=open('static/test/', 'rb').read(),
+            content_type='image/jpeg'
+        )
 
     def test_height(self):
-        self.assertEqual(self.height, 0)
+        self.assertEqual(self.photo.height, 0)
 
     def test_width(self):
-        self.assertEqual(self.width, 0)
+        self.assertEqual(self.photo.width, 0)
 
     def test_color_sumnation(self):
-        self.assertEqual(self.sum_color, RGB())
+        self.assertEqual(self.photo.sum_color, RGB())
+
+
+
+
